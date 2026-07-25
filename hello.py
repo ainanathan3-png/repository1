9725222df724228cd45f8d53782d735c38fdeea5
ainanathan3.png <ainanathan3.@gmail.com>
2026-07-25 17:41:34 -0500

    why

---
+++ hello.py
@@ -0,0 +1,3 @@
+tung= input("How is your day going ")
+if tung == "fine":
+    print("Kim Jong Un is the alpha GOAT (:<) ")
---
+++ story.py
@@ -0,0 +1,7 @@
+subject=input("A little child blasts his power at you, what do you do? A) Dodge B) Secret ")
+if subject== "A":
+    print("You are trash lol AHHAHAHAHHAHHA")
+if subject == "B":
+    dih = input("now what is the actual secret")
+if subject == "summon the power of diddy and use your baby oil power":
+    print("Toes who knows *fire emoji* *cold emoji* *enrique (slowed and reverbed)")
